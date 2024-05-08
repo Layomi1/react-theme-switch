b42d6540139ed902cacd7a738b596ca2f0be6fd8
Layomi1 <107419093+Layomi1@users.noreply.github.com>
2024-05-08 15:34:58 +0100

    removing React definition that was not used

--- src/components/UseLocalStorage.jsx
+++ src/components/UseLocalStorage.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 
 const UseLocalStorage = (key, defaultvalue) => {
   const [value, setValue] = useState(() => {
